9ac492ce1cf14fc747bdec5fa774a856a31d5e07
Michael Li <thisislimichael@gmail.com>
2021-12-13 22:05:58 -0800

    added connection to server

---
+++ server.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Macbookpro1',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
